de94801697745ab4fa72873c87a4ded948e87104
smallnest <smallnest@gmail.com>
2016-05-19 11:33:43 +0800

    add client plugin feature

--- client_plugin.go
+++ client_plugin.go
@@ -1,5 +1,7 @@
 package betterrpc
 
+import "net/rpc"
+
 // ClientPluginContainer implements IPluginContainer interface.
 type ClientPluginContainer struct {
 	plugins []IPlugin
@@ -76,7 +78,91 @@ func (p *ClientPluginContainer) GetAll() []IPlugin {
 	return p.plugins
 }
 
+// DoPreReadResponseHeader invokes DoPreReadResponseHeader plugin.
+func (p *ClientPluginContainer) DoPreReadResponseHeader(r *rpc.Response) {
+	for i := range p.plugins {
+		if plugin, ok := p.plugins[i].(IPreReadResponseHeaderPlugin); ok {
+			plugin.PreReadResponseHeader(r)
+		}
+	}
+}
+
+// DoPostReadResponseHeader invokes DoPostReadResponseHeader plugin.
+func (p *ClientPluginContainer) DoPostReadResponseHeader(r *rpc.Response) {
+	for i := range p.plugins {
+		if plugin, ok := p.plugins[i].(IPostReadResponseHeaderPlugin); ok {
+			plugin.PostReadResponseHeader(r)
+		}
+	}
+}
+
+// DoPreReadResponseBody invokes DoPreReadResponseBody plugin.
+func (p *ClientPluginContainer) DoPreReadResponseBody(body interface{}) {
+	for i := range p.plugins {
+		if plugin, ok := p.plugins[i].(IPreReadResponseBodyPlugin); ok {
+			plugin.PreReadResponseBody(body)
+		}
+	}
+}
+
+// DoPostReadResponseBody invokes DoPostReadResponseBody plugin.
+func (p *ClientPluginContainer) DoPostReadResponseBody(body interface{}) {
+	for i := range p.plugins {
+		if plugin, ok := p.plugins[i].(IPostReadResponseBodyPlugin); ok {
+			plugin.PostReadResponseBody(body)
+		}
+	}
+}
+
+// DoPreWriteRequest invokes DoPreWriteRequest plugin.
+func (p *ClientPluginContainer) DoPreWriteRequest(r *rpc.Request, body interface{}) {
+	for i := range p.plugins {
+		if plugin, ok := p.plugins[i].(IPreWriteRequestPlugin); ok {
+			plugin.PreWriteRequest(r, body)
+		}
+	}
+}
+
+// DoPostWriteRequest invokes DoPostWriteRequest plugin.
+func (p *ClientPluginContainer) DoPostWriteRequest(r *rpc.Request, body interface{}) {
+	for i := range p.plugins {
+		if plugin, ok := p.plugins[i].(IPostWriteRequestPlugin); ok {
+			plugin.PostWriteRequest(r, body)
+		}
+	}
+}
+
 type (
+
+	//IPreReadResponseHeaderPlugin represents .
+	IPreReadResponseHeaderPlugin interface {
+		PreReadResponseHeader(*rpc.Response)
+	}
+
+	//IPostReadResponseHeaderPlugin represents .
+	IPostReadResponseHeaderPlugin interface {
+		PostReadResponseHeader(*rpc.Response)
+	}
+
+	//IPreReadResponseBodyPlugin represents .
+	IPreReadResponseBodyPlugin interface {
+		PreReadResponseBody(interface{})
+	}
+
+	//IPostReadResponseBodyPlugin represents .
+	IPostReadResponseBodyPlugin interface {
+		PostReadResponseBody(interface{})
+	}
+
+	//IPreWriteRequestPlugin represents .
+	IPreWriteRequestPlugin interface {
+		PreWriteRequest(*rpc.Request, interface{})
+	}
+
+	//IPostWriteRequestPlugin represents .
+	IPostWriteRequestPlugin interface {
+		PostWriteRequest(*rpc.Request, interface{})
+	}
 
 	//IClientPluginContainer represents a plugin container that defines all methods to manage plugins.
 	//And it also defines all extension points.
@@ -87,5 +173,13 @@ type (
 		GetDescription(plugin IPlugin) string
 		GetByName(pluginName string) IPlugin
 		GetAll() []IPlugin
+
+		DoPreReadResponseHeader(*rpc.Response)
+		DoPostReadResponseHeader(*rpc.Response)
+		DoPreReadResponseBody(interface{})
+		DoPostReadResponseBody(interface{})
+
+		DoPreWriteRequest(*rpc.Request, interface{})
+		DoPostWriteRequest(*rpc.Request, interface{})
 	}
 )
