003cb7432542f7a9a885c6a726cc9eb4eb5f88fd
smallnest <smallnest@gmail.com>
2016-05-31 10:54:00 +0800

    add client pool example

---
+++ _examples/cache/client.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/smallnest/rpcx"
+	"github.com/smallnest/rpcx/clientselector"
+)
+
+type Args struct {
+	A int `msg:"a"`
+	B int `msg:"b"`
+}
+
+type Reply struct {
+	C int `msg:"c"`
+}
+
+func main() {
+	server1 := clientselector.ServerPair{Network: "tcp", Address: "127.0.0.1:8972"}
+	server2 := clientselector.ServerPair{Network: "tcp", Address: "127.0.0.1:8973"}
+
+	servers := []clientselector.ServerPair{server1, server2}
+
+	s := clientselector.NewMultiClientSelector(servers, rpcx.RandomSelect, 10*time.Second)
+
+	clientPool := &sync.Pool{
+		New: func() interface{} {
+			return rpcx.NewClient(s)
+		},
+	}
+	for i := 0; i < 10; i++ {
+		callServer(clientPool, s)
+	}
+}
+
+func callServer(clientPool *sync.Pool, s rpcx.ClientSelector) {
+	client := clientPool.Get().(*rpcx.Client)
+
+	args := &Args{7, 8}
+	var reply Reply
+	err := client.Call("Arith.Mul", args, &reply)
+	if err != nil {
+		fmt.Printf("error for Arith: %d*%d, %v \n", args.A, args.B, err)
+	} else {
+		fmt.Printf("Arith: %d*%d=%d, client: %p \n", args.A, args.B, reply.C, client)
+	}
+
+	clientPool.Put(client)
+}
---
+++ _examples/cache/server.go
@@ -0,0 +1,44 @@
+package main
+
+import "github.com/smallnest/rpcx"
+
+type Args struct {
+	A int `msg:"a"`
+	B int `msg:"b"`
+}
+
+type Reply struct {
+	C int `msg:"c"`
+}
+
+type Arith int
+
+func (t *Arith) Mul(args *Args, reply *Reply) error {
+	reply.C = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Error(args *Args, reply *Reply) error {
+	panic("ERROR")
+}
+
+type Arith2 int
+
+func (t *Arith2) Mul(args *Args, reply *Reply) error {
+	reply.C = args.A * args.B * 10
+	return nil
+}
+
+func (t *Arith2) Error(args *Args, reply *Reply) error {
+	panic("ERROR")
+}
+
+func main() {
+	server1 := rpcx.NewServer()
+	server1.RegisterName("Arith", new(Arith))
+	server1.Start("tcp", "127.0.0.1:8972")
+
+	server2 := rpcx.NewServer()
+	server2.RegisterName("Arith", new(Arith2))
+	server2.Serve("tcp", "127.0.0.1:8973")
+}
