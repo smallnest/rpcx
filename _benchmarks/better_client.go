afad6dc6ec37351af704e748ffcc782db06b7970
smallnest <smallnest@gmail.com>
2016-09-06 16:04:35 +0800

    add Percentile 99.9 stat for benchmark

--- _benchmarks/better_client.go
+++ _benchmarks/better_client.go
@@ -89,13 +89,15 @@ func main() {
 	median, _ := stats.Median(totalD2)
 	max, _ := stats.Max(totalD2)
 	min, _ := stats.Min(totalD2)
+	p99, _ := stats.Percentile(totalD2, 99.9)
 
 	fmt.Printf("sent     requests    : %d\n", n*m)
 	fmt.Printf("received requests    : %d\n", atomic.LoadUint64(&trans))
 	fmt.Printf("received requests_OK : %d\n", atomic.LoadUint64(&transOK))
 	fmt.Printf("throughput  (TPS)    : %d\n", int64(n*m)*1000/totalT)
-	fmt.Printf("mean: %.f ns, median: %.f ns, max: %.f ns, min: %.f ns\n", mean, median, max, min)
-	fmt.Printf("mean: %d ms, median: %d ms, max: %d ms, min: %d ms\n", int64(mean/1000000), int64(median/1000000), int64(max/1000000), int64(min/1000000))
+	fmt.Printf("mean: %.f ns, median: %.f ns, max: %.f ns, min: %.f ns, p99: %.f\n", mean, median, max, min, p99)
+	fmt.Printf("mean: %d ms, median: %d ms, max: %d ms, min: %d ms, 99.9%: %d\n",
+		int64(mean/1000000), int64(median/1000000), int64(max/1000000), int64(min/1000000), int64(p99/1000000))
 
 	closeClients(clients)
 }
