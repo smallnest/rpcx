f0dfebc908d7b8835a5a9563e9de950158921ae9
smallnest <smallnest@gmail.com>
2016-09-23 22:30:47 +0800

    add tls example

---
+++ _examples/tls/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+
+	"github.com/smallnest/rpcx"
+)
+
+type Args struct {
+	A int `msg:"a"`
+	B int `msg:"b"`
+}
+
+type Reply struct {
+	C int `msg:"c"`
+}
+
+type Arith int
+
+func (t *Arith) Mul(args *Args, reply *Reply) error {
+	reply.C = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Error(args *Args, reply *Reply) error {
+	panic("ERROR")
+}
+
+func main() {
+	server := rpcx.NewServer()
+	server.RegisterName("Arith", new(Arith))
+
+	cert, err := tls.LoadX509KeyPair("server.pem", "server.key")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	config := &tls.Config{Certificates: []tls.Certificate{cert}}
+
+	server.ServeTLS("tcp", "127.0.0.1:8972", config)
+}
