fe2a9e015be357e591d292635091007a428c3f6c
smallnest <smallnest@gmail.com>
2016-11-10 17:08:40 +0800

    #63 calculate correct time for requests by internal seqs

--- plugin/metrics.go
+++ plugin/metrics.go
@@ -3,6 +3,8 @@ package plugin
 import (
 	"net"
 	"net/rpc"
+	"sync"
+	"time"
 
 	"github.com/rcrowley/go-metrics"
 )
@@ -10,52 +12,87 @@ import (
 //MetricsPlugin collects metrics of a rpc server
 type MetricsPlugin struct {
 	Registry metrics.Registry
-	seqs     map[uint64]int64
+	timeSeqMap     map[uint64]int64
+	mapLock sync.RWMutex
+	internalSeq uint64
+	internalSeqMap map[uint64]uint64
 }
+
+
+
 
 //NewMetricsPlugin creates a new MetricsPlugirn
 func NewMetricsPlugin() *MetricsPlugin {
-	return &MetricsPlugin{Registry: metrics.NewRegistry(), seqs: make(map[uint64]int64, 100)}
+	return &MetricsPlugin{Registry: metrics.NewRegistry(), timeSeqMap: make(map[uint64]int64, 100),internalSeqMap: make(map[uint64]uint64, 100)}
 }
 
 // Register handles registering event.
-func (plugin *MetricsPlugin) Register(name string, rcvr interface{}, metadata ...string) error {
-	serviceCounter := metrics.GetOrRegisterCounter("serviceCounter", plugin.Registry)
+func (p *MetricsPlugin) Register(name string, rcvr interface{}, metadata ...string) error {
+	serviceCounter := metrics.GetOrRegisterCounter("serviceCounter", p.Registry)
 	serviceCounter.Inc(1)
 	return nil
 }
 
 // HandleConnAccept handles connections from clients
-func (plugin *MetricsPlugin) HandleConnAccept(net.Conn) bool {
-	clientMeter := metrics.GetOrRegisterMeter("clientMeter", plugin.Registry)
+func (p *MetricsPlugin) HandleConnAccept(net.Conn) bool {
+	clientMeter := metrics.GetOrRegisterMeter("clientMeter", p.Registry)
 	clientMeter.Mark(1)
 	return true
 }
 
+// PreReadRequestHeader marks start time of calling service
+func (p *MetricsPlugin) PreReadRequestHeader(r *rpc.Request) error {
+	p.mapLock.Lock()
+	defer p.mapLock.Unlock()
+	//replace seq with internalSeq
+	p.internalSeqMap[p.internalSeq] = r.Seq
+	r.Seq = p.internalSeq
+	p.internalSeq++
+	p.timeSeqMap[r.Seq] = time.Now().UnixNano()
+	return nil
+}
+
 // PostReadRequestHeader counts read
-func (plugin *MetricsPlugin) PostReadRequestHeader(r *rpc.Request) error {
+func (p *MetricsPlugin) PostReadRequestHeader(r *rpc.Request) error {
 	if r.ServiceMethod == "" {
 		return nil
 	}
 
-	m := metrics.GetOrRegisterMeter("service_"+r.ServiceMethod+"_Read_Counter", plugin.Registry)
+	m := metrics.GetOrRegisterMeter("service_"+r.ServiceMethod+"_Read_Counter", p.Registry)
 	m.Mark(1)
 	return nil
 }
 
 // PostWriteResponse count write
-func (plugin *MetricsPlugin) PostWriteResponse(r *rpc.Response, body interface{}) error {
+func (p *MetricsPlugin) PostWriteResponse(r *rpc.Response, body interface{}) error {
 	if r.ServiceMethod == "" {
 		return nil
 	}
 
-	m := metrics.GetOrRegisterMeter("service_"+r.ServiceMethod+"_Write_Counter", plugin.Registry)
+	m := metrics.GetOrRegisterMeter("service_"+r.ServiceMethod+"_Write_Counter", p.Registry)
 	m.Mark(1)
+
+	p.mapLock.RLock()
+	s := r.Seq
+	t := p.timeSeqMap[s]
+	r.Seq = p.internalSeqMap[s]
+	delete(p.internalSeqMap,s)
+	p.mapLock.RUnlock()
+
+	if t > 0 {
+		t = time.Now().UnixNano() - t
+		if t < 30*time.Minute.Nanoseconds() { //it is impossible that calltime exceeds 30 minute
+			//Historgram
+			h := metrics.GetOrRegisterHistogram("service_"+r.ServiceMethod+"_CallTime", p.Registry,
+				metrics.NewExpDecaySample(1028, 0.015))
+			h.Update(t)
+		}
+	}
 
 	return nil
 }
 
 // Name return name of this plugin.
-func (plugin *MetricsPlugin) Name() string {
+func (p *MetricsPlugin) Name() string {
 	return "MetricsPlugin"
 }
