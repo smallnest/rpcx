1e37759670db791ca3712ef04f9e7717e93a59de
Jason E. Aten <j.e.aten@gmail.com>
2017-07-29 03:55:38 -0500

    beginnings of server side QUIC support

--- server_unix.go
+++ server_unix.go
@@ -3,14 +3,17 @@
 package rpcx
 
 import (
+	"crypto/tls"
 	"net"
 
 	reuseport "github.com/kavu/go_reuseport"
+	quicconn "github.com/marten-seemann/quic-conn"
 	kcp "github.com/xtaci/kcp-go"
 )
 
-// block can be nil if the caller wishes to skip encryption.
-func makeListener(network, address string, block kcp.BlockCrypt) (ln net.Listener, err error) {
+// block can be nil if the caller wishes to skip encryption in kcp.
+// tlsConfig can be nil iff we are not using network "quic".
+func makeListener(network, address string, block kcp.BlockCrypt, tlsConfig *tls.Config) (ln net.Listener, err error) {
 	switch network {
 	case "kcp":
 		ln, err = kcp.ListenWithOptions(address, block, 10, 3)
@@ -22,6 +25,8 @@ func makeListener(network, address string, block kcp.BlockCrypt) (ln net.Listene
 		}
 
 		ln, err = reuseport.NewReusablePortListener(network, address)
+	case "quic":
+		ln, err = quicconn.Listen("udp", address, tlsConfig)
 	default: //tcp
 		ln, err = net.Listen(network, address)
 	}
