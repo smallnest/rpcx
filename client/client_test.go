8eb4b17903cf57735c2cccf86092220c52f13c59
smallnest <smallnest@gmail.com>
2017-10-09 18:48:51 +0800

    add messagepack and protobuf tests

--- client/client_test.go
+++ client/client_test.go
@@ -25,9 +25,17 @@ func (t *Arith) Mul(ctx context.Context, args *Args, reply *Reply) error {
 	return nil
 }
 
+type PBArith int
+
+func (t *PBArith) Mul(ctx context.Context, args *ProtoArgs, reply *ProtoReply) error {
+	reply.C = args.A * args.B
+	return nil
+}
+
 func TestClient_IT(t *testing.T) {
 	server := server.Server{}
 	server.RegisterName("Arith", new(Arith))
+	server.RegisterName("PBArith", new(PBArith))
 	go server.Serve("tcp", "127.0.0.1:0")
 	defer server.Close()
 	time.Sleep(500 * time.Millisecond)
@@ -63,5 +71,32 @@ func TestClient_IT(t *testing.T) {
 	err = client.Call(context.Background(), "Arith", "Add", args, reply)
 	if err == nil {
 		t.Fatal("expect an error but got nil")
+	}
+
+	client.SerializeType = protocol.MsgPack
+	reply = &Reply{}
+	err = client.Call(context.Background(), "Arith", "Mul", args, reply)
+	if err != nil {
+		t.Fatalf("failed to call: %v", err)
+	}
+
+	if reply.C != 200 {
+		t.Fatalf("expect 200 but got %d", reply.C)
+	}
+
+	client.SerializeType = protocol.ProtoBuffer
+
+	pbArgs := &ProtoArgs{
+		A: 10,
+		B: 20,
+	}
+	pbReply := &ProtoReply{}
+	err = client.Call(context.Background(), "PBArith", "Mul", pbArgs, pbReply)
+	if err != nil {
+		t.Fatalf("failed to call: %v", err)
+	}
+
+	if pbReply.C != 200 {
+		t.Fatalf("expect 200 but got %d", pbReply.C)
 	}
 }
