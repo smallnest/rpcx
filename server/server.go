be6e9d7c12d84c6148ea5874e6b7ec71bc80c7bb
smallnest <smallnest@gmail.com>
2017-10-24 15:43:40 +0800

    limit output stack size

--- server/server.go
+++ server/server.go
@@ -211,7 +211,11 @@ func (s *Server) serveConn(conn net.Conn) {
 		if err := recover(); err != nil {
 			const size = 64 << 10
 			buf := make([]byte, size)
-			buf = buf[:runtime.Stack(buf, false)]
+			ss := runtime.Stack(buf, false)
+			if ss > size {
+				ss = size
+			}
+			buf = buf[:ss]
 			log.Errorf("serving %s panic error: %s, stack:\n %s", conn.RemoteAddr(), err, buf)
 		}
 		s.mu.Lock()
