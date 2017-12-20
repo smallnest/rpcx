3b04bb65d3bce252ddf9dbef767fa9f34ea66303
smallnest <smallnest@gmail.com>
2017-12-20 11:16:24 +0800

    use clear error flag

--- server/service.go
+++ server/service.go
@@ -301,7 +301,7 @@ func suitableMethods(typ reflect.Type, reportErr bool) map[string]*methodType {
 func (s *service) call(ctx context.Context, mtype *methodType, argv, replyv reflect.Value) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
-			err = fmt.Errorf("internal error: %v", r)
+			err = fmt.Errorf("[service internal error]: %v", r)
 		}
 	}()
 
@@ -320,7 +320,7 @@ func (s *service) call(ctx context.Context, mtype *methodType, argv, replyv refl
 func (s *service) callForFunction(ctx context.Context, ft *functionType, argv, replyv reflect.Value) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
-			err = fmt.Errorf("internal error: %v", r)
+			err = fmt.Errorf("[service internal error]: %v", r)
 		}
 	}()
 
