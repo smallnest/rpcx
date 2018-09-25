fa882ccb86dbbf8351fddb8ac7f05e2cbae8d6de
smallnest <smallnest@gmail.com>
2018-09-25 11:52:39 +0800

    add a method to get all active client connections

--- server/server.go
+++ server/server.go
@@ -114,6 +114,15 @@ func (s *Server) Address() net.Addr {
 	return s.ln.Addr()
 }
 
+func (s *Server) ActiveClientConn() []net.Conn {
+	var result []net.Conn
+
+	for clientConn, _ := range s.activeConn {
+		result = append(result, clientConn)
+	}
+	return result
+}
+
 // SendMessage a request to the specified client.
 // The client is designated by the conn.
 // conn can be gotten from context in services:
