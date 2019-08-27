29f7965d6a1b312dfc834f131330bc1695e3ebc9
smallnest <smallnest@gmail.com>
2019-08-27 10:49:21 +0800

    #352 add etcdv3 registry

---
+++ client/etcdv3_discovery.go
@@ -0,0 +1,234 @@
+// +build etcd
+
+package client
+
+import (
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/docker/libkv"
+	"github.com/docker/libkv/store"
+	etcd "github.com/smallnest/libkv-etcdv3-store"
+	"github.com/smallnest/rpcx/log"
+)
+
+func init() {
+	etcd.Register()
+}
+
+// EtcdV3Discovery is a etcd service discovery.
+// It always returns the registered servers in etcd.
+type EtcdV3Discovery struct {
+	basePath string
+	kv       store.Store
+	pairs    []*KVPair
+	chans    []chan []*KVPair
+	mu       sync.Mutex
+
+	// -1 means it always retry to watch until zookeeper is ok, 0 means no retry.
+	RetriesAfterWatchFailed int
+
+	stopCh chan struct{}
+}
+
+// NewEtcdV3Discovery returns a new EtcdV3Discovery.
+func NewEtcdV3Discovery(basePath string, servicePath string, etcdAddr []string, options *store.Config) ServiceDiscovery {
+	kv, err := libkv.NewStore(etcd.ETCDV3, etcdAddr, options)
+	if err != nil {
+		log.Infof("cannot create store: %v", err)
+		panic(err)
+	}
+
+	return NewEtcdV3DiscoveryStore(basePath+"/"+servicePath, kv)
+}
+
+// NewEtcdV3DiscoveryStore return a new EtcdV3Discovery with specified store.
+func NewEtcdV3DiscoveryStore(basePath string, kv store.Store) ServiceDiscovery {
+	if len(basePath) > 1 && strings.HasSuffix(basePath, "/") {
+		basePath = basePath[:len(basePath)-1]
+	}
+
+	d := &EtcdV3Discovery{basePath: basePath, kv: kv}
+	d.stopCh = make(chan struct{})
+
+	ps, err := kv.List(basePath)
+	if err != nil {
+		log.Infof("cannot get services of from registry: %v, err: %v", basePath, err)
+		panic(err)
+	}
+	var pairs = make([]*KVPair, 0, len(ps))
+	var prefix string
+	for _, p := range ps {
+		if prefix == "" {
+			if strings.HasPrefix(p.Key, "/") {
+				if strings.HasPrefix(d.basePath, "/") {
+					prefix = d.basePath + "/"
+				} else {
+					prefix = "/" + d.basePath + "/"
+				}
+			} else {
+				if strings.HasPrefix(d.basePath, "/") {
+					prefix = d.basePath[1:] + "/"
+				} else {
+					prefix = d.basePath + "/"
+				}
+			}
+		}
+		if p.Key == prefix[:len(prefix)-1] {
+			continue
+		}
+		k := strings.TrimPrefix(p.Key, prefix)
+		pairs = append(pairs, &KVPair{Key: k, Value: string(p.Value)})
+	}
+	d.pairs = pairs
+	d.RetriesAfterWatchFailed = -1
+
+	go d.watch()
+	return d
+}
+
+// NewEtcdV3DiscoveryTemplate returns a new EtcdV3Discovery template.
+func NewEtcdV3DiscoveryTemplate(basePath string, etcdAddr []string, options *store.Config) ServiceDiscovery {
+	if len(basePath) > 1 && strings.HasSuffix(basePath, "/") {
+		basePath = basePath[:len(basePath)-1]
+	}
+
+	kv, err := libkv.NewStore(store.ETCD, etcdAddr, options)
+	if err != nil {
+		log.Infof("cannot create store: %v", err)
+		panic(err)
+	}
+
+	return &EtcdV3Discovery{basePath: basePath, kv: kv}
+}
+
+// Clone clones this ServiceDiscovery with new servicePath.
+func (d EtcdV3Discovery) Clone(servicePath string) ServiceDiscovery {
+	return NewEtcdV3DiscoveryStore(d.basePath+"/"+servicePath, d.kv)
+}
+
+// GetServices returns the servers
+func (d EtcdV3Discovery) GetServices() []*KVPair {
+	return d.pairs
+}
+
+// WatchService returns a nil chan.
+func (d *EtcdV3Discovery) WatchService() chan []*KVPair {
+	ch := make(chan []*KVPair, 10)
+	d.chans = append(d.chans, ch)
+	return ch
+}
+
+func (d *EtcdV3Discovery) RemoveWatcher(ch chan []*KVPair) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	var chans []chan []*KVPair
+	for _, c := range d.chans {
+		if c == ch {
+			continue
+		}
+
+		chans = append(chans, c)
+	}
+
+	d.chans = chans
+}
+
+func (d *EtcdV3Discovery) watch() {
+	for {
+		var err error
+		var c <-chan []*store.KVPair
+		var tempDelay time.Duration
+
+		retry := d.RetriesAfterWatchFailed
+		for d.RetriesAfterWatchFailed == -1 || retry > 0 {
+			c, err = d.kv.WatchTree(d.basePath, nil)
+			if err != nil {
+				if d.RetriesAfterWatchFailed > 0 {
+					retry--
+				}
+				if tempDelay == 0 {
+					tempDelay = 1 * time.Second
+				} else {
+					tempDelay *= 2
+				}
+				if max := 30 * time.Second; tempDelay > max {
+					tempDelay = max
+				}
+				log.Warnf("can not watchtree (with retry %d, sleep %v): %s: %v", retry, tempDelay, d.basePath, err)
+				time.Sleep(tempDelay)
+				continue
+			}
+			break
+		}
+
+		if err != nil {
+			log.Errorf("can't watch %s: %v", d.basePath, err)
+			return
+		}
+
+	readChanges:
+		for {
+			select {
+			case <-d.stopCh:
+				log.Info("discovery has been closed")
+				return
+			case ps := <-c:
+				if ps == nil {
+					break readChanges
+				}
+				var pairs []*KVPair // latest servers
+				var prefix string
+				for _, p := range ps {
+					if prefix == "" {
+						if strings.HasPrefix(p.Key, "/") {
+							if strings.HasPrefix(d.basePath, "/") {
+								prefix = d.basePath + "/"
+							} else {
+								prefix = "/" + d.basePath + "/"
+							}
+						} else {
+							if strings.HasPrefix(d.basePath, "/") {
+								prefix = d.basePath[1:] + "/"
+							} else {
+								prefix = d.basePath + "/"
+							}
+						}
+					}
+					if p.Key == prefix[:len(prefix)-1] {
+						continue
+					}
+
+					k := strings.TrimPrefix(p.Key, prefix)
+					pairs = append(pairs, &KVPair{Key: k, Value: string(p.Value)})
+				}
+				d.pairs = pairs
+
+				for _, ch := range d.chans {
+					ch := ch
+					go func() {
+						defer func() {
+							if r := recover(); r != nil {
+
+							}
+						}()
+
+						select {
+						case ch <- pairs:
+						case <-time.After(time.Minute):
+							log.Warn("chan is full and new change has been dropped")
+						}
+					}()
+				}
+			}
+		}
+
+		log.Warn("chan is closed and will rewatch")
+	}
+}
+
+func (d *EtcdV3Discovery) Close() {
+	close(d.stopCh)
+}
