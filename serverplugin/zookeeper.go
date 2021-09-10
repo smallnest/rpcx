de24eefa9039f06d49df7f8f8119e0eb3413507a
chuwt <weitaochu@gmail.com>
2021-09-10 10:33:43 +0800

    fix: add missing calls to Ticker.Stop

--- serverplugin/zookeeper.go
+++ serverplugin/zookeeper.go
@@ -73,8 +73,10 @@ func (p *ZooKeeperRegisterPlugin) Start() error {
 	}
 
 	if p.UpdateInterval > 0 {
-		ticker := time.NewTicker(p.UpdateInterval)
 		go func() {
+			ticker := time.NewTicker(p.UpdateInterval)
+
+			defer ticker.Stop()
 			defer p.kv.Close()
 
 			// refresh service TTL
