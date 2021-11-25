3abe01e2e4e1099feac2c1ae75d5feaa93120565
Junlong <fu4ng@foxmail.com>
2021-11-25 18:36:01 +0800

    fix typo

--- client/oneclient.go
+++ client/oneclient.go
@@ -335,7 +335,7 @@ func (c *OneClient) Stream(ctx context.Context, meta map[string]string) (net.Con
 	return xclient.Stream(ctx, meta)
 }
 
-// Close closes all xclients and its underlying connnections to services.
+// Close closes all xclients and its underlying connections to services.
 func (c *OneClient) Close() error {
 	var result error
 
