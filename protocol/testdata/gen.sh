3ceb23012e3cc1a6caaabd71665c06e59a5244a1
chengy <chengy@2wb.com>
2024-05-30 15:39:33 +0800

    fix issues #855

--- protocol/testdata/gen.sh
+++ protocol/testdata/gen.sh
@@ -1,5 +1,5 @@
 # curl -O https://raw.githubusercontent.com/rpcxio/rpcx-benchmark/master/proto/benchmark.proto
 
 # generate .go files from IDL
-protoc --go_out=./ ./benchmark.proto
 
+protoc -I.  --go_out=. --go_opt=module="github.com/smallnest/rpcx/protocol/testdata"   ./benchmark.proto
